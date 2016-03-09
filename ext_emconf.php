1e889ba112c03d24c60acd47b4c06206b1e1acf3
Stanislas Rolland <typo3@sjbr.ca>
2016-03-09 16:54:27 -0500

    [TASK] Update version to 6.3.2
    Change-Id: Ibe3d2ccbb4c21c33a63a937cc89a4aabf448d0ab

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Language Selection',
 	'description' => 'A plugin to display a list of languages to select from. Clicking on a language links to the corresponding version of the page.',
 	'category' => 'plugin',
-	'version' => '6.3.1',
+	'version' => '6.3.2',
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -17,7 +17,6 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'SJBR',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.3.0-0.0.0',
 			'typo3' => '6.2.0-7.6.99',
 			'static_info_tables' => '6.3.9-6.3.99'
 		),
