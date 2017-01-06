65f3ba8659628f3fe9c77012c2d5f47acd63fe20
Stanislas Rolland <typo3@sjbr.ca>
2017-01-06 02:18:28 -0500

    [FEATURE] Compatibility with TYPO3 version 8.x and require TYPO3 7 LTS
    Resolves: #78340
    Change-Id: I0d2c739092428bcf90435fc558a4018f4568d389
    Reviewed-on: https://review.typo3.org/51160
    Reviewed-by: Stanislas Rolland <typo3@sjbr.ca>
    Tested-by: Stanislas Rolland <typo3@sjbr.ca>

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'SJBR',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.6.99',
+			'typo3' => '7.6.0-8.99.99',
 			'static_info_tables' => '6.3.9-6.3.99'
 		),
 		'conflicts' => array(),
