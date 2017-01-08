f12beeef7b980fce93c890ae86e5f30f545d79fc
Stanislas Rolland <typo3@sjbr.ca>
2017-01-08 00:06:11 -0500

    [TASK] Cleanup extension configuration files
    Change-Id: I9fe6405b65a5409ae4283bc9226d429c1d81e291
    Reviewed-on: https://review.typo3.org/51188
    Reviewed-by: Stanislas Rolland <typo3@sjbr.ca>
    Tested-by: Stanislas Rolland <typo3@sjbr.ca>

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,17 +4,16 @@ defined('TYPO3_MODE') or die();
 // Configuring the language menu plugin
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
 	// The extension name (in UpperCamelCase) or the extension key (in lower_underscore)
-	'SJBR.' . $_EXTKEY,
+	'SJBR.sr_language_menu',
 	// A unique name of the plugin in UpperCamelCase
 	'LanguageMenu',
 	// An array holding the controller-action-combinations that are accessible
 	// The first controller and its first action will be the default
-	array (
+	[
 		'Menu' => 'index,redirect'
-	),
+	],
 	// An array of non-cachable controller-action-combinations (they must already be enabled)
-	array(
-	),
+	[],
 	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
 );
 
@@ -23,10 +22,10 @@ $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\
 $iconRegistry->registerIcon(
 	'tx-srlanguagemenu-language',
 	\TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-	['source' => 'EXT:' . $_EXTKEY . '/Resources/Public/Images/language.svg']
+	['source' => 'EXT:sr_language_menu/Resources/Public/Images/language.svg']
 );
 
 if (TYPO3_MODE === 'BE') {
 	// Include page TS configuration for new element wizard
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/modWizards.txt">');
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:sr_language_menu/Configuration/PageTS/modWizards.txt">');
 }
