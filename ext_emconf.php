aeaf026a626d61d9995598b30a6c6ee879d5c6d2
Stanislas Rolland <typo3@sjbr.ca>
2017-01-24 12:23:35 -0500

    [TASK] Update extension requirements
    Change-Id: Ife01e8b216056192fc52b5c8dedb992678e9ce97
    Reviewed-on: https://review.typo3.org/51408
    Reviewed-by: Stanislas Rolland <typo3@sjbr.ca>
    Tested-by: Stanislas Rolland <typo3@sjbr.ca>

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '7.6.0-8.99.99',
-			'static_info_tables' => '6.3.9-6.3.99'
+			'static_info_tables' => '6.4.2-6.4.99'
 		),
 		'conflicts' => array(),
 		'suggests' => array()
