c5ab710b1d252f542c413d8ba65e7c2addba2cd3
Stanislas Rolland <typo3@sjbr.ca>
2018-10-18 22:02:32 -0400

    [BUGFIX] Incorrect keywords format for composer
    Resolves: #86693
    Change-Id: If4805948400cba27a713f534d89e603cca418bf2
    Reviewed-on: https://review.typo3.org/58656
    Reviewed-by: Stanislas Rolland <typo3@sjbr.ca>
    Tested-by: Stanislas Rolland <typo3@sjbr.ca>

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,11 +3,11 @@
  * Extension Manager configuration file for ext "sr_language_menu".
  */
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
 	'title' => 'Language Selection',
-	'description' => 'A plugin to display a list of languages to select from. Clicking on a language links to the corresponding version of the page.',
+	'description' => 'A TYPO3 plugin to display a list of languages to select from. Clicking on a language links to the corresponding version of the page.',
 	'category' => 'plugin',
-	'version' => '6.5.0',
+	'version' => '6.5.1',
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -15,12 +15,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Stanislas Rolland',
 	'author_email' => 'typo3(arobas)sjbr.ca',
 	'author_company' => 'SJBR',
-	'constraints' => array(
-		'depends' => array(
+	'constraints' => [
+		'depends' => [
 			'typo3' => '9.5.0-9.5.99',
 			'static_info_tables' => '6.7.0-6.7.99'
-		),
-		'conflicts' => array(),
-		'suggests' => array()
-	)
-);
+		],
+		'conflicts' => [],
+		'suggests' => []
+	]
+];
